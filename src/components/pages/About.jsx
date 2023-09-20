03a5d7ca577ec920d20a1c9add516b15ed9bdf67
Max Horstmann <mahor575@gmail.com.com>
2023-09-20 12:32:19 +0200

    added Tech-Stack

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,5 +1,16 @@
 import DefaultLayout from '../defaultLayout/DefaultLayout';
 import max from './pics/max.jpg';
+import html from './icons/html.png';
+import js from './icons/js.png';
+import css from './icons/css.png';
+import devops from './icons/devops.png';
+import git from './icons/git.png';
+import jest from './icons/jest.png';
+import json from './icons/json.png';
+import mongodb from './icons/mongodb.png';
+import nodejs from './icons/nodejs.png';
+import postgresql from './icons/postgresql.png';
+import react from './icons/react.png';
 
 const About = () => {
   return (
@@ -33,7 +44,93 @@ const About = () => {
             neque esse sit!
           </p>
         </div>
-        <div>Download CV?</div>
+        <div>
+          <h2>Skill-Stack</h2>
+          <div className="stack">
+            <img
+              src={html}
+              alt="html-icon"
+            />
+            <h3>HTML</h3>
+          </div>
+          <div className="stack">
+            <img
+              src={css}
+              alt="icon"
+            />
+            <h3>CSS</h3>
+          </div>
+          <div className="stack">
+            <img
+              src={js}
+              alt="icon"
+            />
+            <h3>Javascript</h3>
+          </div>
+          <div className="stack">
+            <img
+              src={react}
+              alt="icon"
+            />
+            <h3>React</h3>
+          </div>
+          <div className="stack">
+            <img
+              src=""
+              alt="icon"
+            />
+            <h3>ExpressJS</h3>
+          </div>
+          <div className="stack">
+            <img
+              src={nodejs}
+              alt="icon"
+            />
+            <h3>NodeJS</h3>
+          </div>
+          <div className="stack">
+            <img
+              src={postgresql}
+              alt="icon"
+            />
+            <h3>SQL</h3>
+          </div>
+          <div className="stack">
+            <img
+              src={mongodb}
+              alt="icon"
+            />
+            <h3>NoSQL</h3>
+          </div>
+          <div className="stack">
+            <img
+              src={git}
+              alt="icon"
+            />
+            <h3>Git & GitHub</h3>
+          </div>
+          <div className="stack">
+            <img
+              src={json}
+              alt="icon"
+            />
+            <h3>Restful APIs</h3>
+          </div>
+          <div className="stack">
+            <img
+              src={devops}
+              alt="icon"
+            />
+            <h3>DevOps</h3>
+          </div>
+          <div className="stack">
+            <img
+              src={jest}
+              alt="icon"
+            />
+            <h3>Software Testing</h3>
+          </div>
+        </div>
       </div>
     </DefaultLayout>
   );
